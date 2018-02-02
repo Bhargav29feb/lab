311621f9b393d063195baded6a1691fbbd92152e
Bhuvan chander Joshi <bhargav29feb@yahoo.com>
2018-02-02 16:20:35 +0530

    Update important.js

--- important.js
+++ important.js
@@ -91,3 +91,22 @@ $(document).ready(function(){
 	});
 });
 </script>
+
+==========================
+Language selecter code
+
+
+<script> 
+
+<div id="google_translate_element"></div>
+<script> 
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'en'
+  }, 'google_translate_element');
+}
+</script>
+<script src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+
+
